e08b5811a1f6f7fcfc1c20c9f95d853e6f05d407
Suvikan <sanledell@gmail.com>
2023-01-11 12:49:14 +0200

    add needed components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { DangerAlertComponent } from './danger-alert/danger-alert.component';
+import { InfoAlertComponent } from './info-alert/info-alert.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SuccessAlertComponent,
+    WarningAlertComponent,
+    DangerAlertComponent,
+    InfoAlertComponent
   ],
   imports: [
     BrowserModule
